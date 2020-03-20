058add2d5aa1d15cca943d6038ea96e2c6a32b93
m0ln1a <technic.dima@gmail.com>
2020-03-20 15:29:09 +0300

    Create Task9.cs

---
+++ university/Tasks/Task9.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(ConvertToBin(number));
+            Console.ReadKey();
+        }
+
+        static int ConvertToBin(int number)
+        {
+            int result = 0, d = 1;
+
+            while (number > 0)
+            {
+                result += (number % 2) * d;
+                number /= 2;
+                d = d * 10;
+            }
+
+            return result;
+        }
+    }
+}
