2f5ba076e8174b9547e31ab74ac7f69de06444b0
m0ln1a <technic.dima@gmail.com>
2020-05-07 20:17:46 +0300

    Update StructLink.cpp

--- StructLink.cpp
+++ StructLink.cpp
@@ -1,75 +1,75 @@
 #include <iostream>
-#include <list>
-#include <iterator>
 
 using namespace std;
 
 struct StructLink
 {
-	int field;
+	int link;
 	struct StructLink* next;
 	struct StructLink* prev;
 };
 
-struct StructLink* inti(int n) // инициализация списка
+struct StructLink* StructInit(int n)
 {
 	struct StructLink* fst;
 	fst = (struct StructLink*)malloc(sizeof(struct StructLink));
-	fst->field = n;
+	fst->link = n;
 	fst->next = NULL;
 	fst->prev = NULL;
+
 	return(fst);
 }
 
-struct StructLink* AddToStart(StructLink* fst) // добавление в начало
+struct StructLink* AddToStart(StructLink* fst)
 {
 	int number;
 	cout << "Введите элемент для добавления в начало списка" << endl;
 	cin >> number;
-	struct StructLink* temp; // буферный кусок
-	temp = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* temp;
+	temp = (struct StructLink*)malloc(sizeof(StructLink));
 
 	while (fst->prev != NULL)
 		fst = fst->prev;
 
-	fst->prev = temp; // fst становится следующим для
-	temp->field = number; // в буферный задаем поле
-	temp->next = fst; // в буферный задаем следующий за ним элемент - p
-	temp->prev = NULL; // в буферный задаем предыдущий нуль
+	fst->prev = temp;
+	temp->link = number;
+	temp->next = fst;
+	temp->prev = NULL;
+
 	return(temp);
 }
 
-struct StructLink* addingend(StructLink* fst) // добавление в конец
+struct StructLink* AddToEnd(StructLink* fst)
 {
 	int number;
 	cout << "Введите элемент для добавления в конец списка" << endl;
 	cin >> number;
-	struct StructLink* temp; // буферный кусок
-	temp = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* temp;
+	temp = (struct StructLink*)malloc(sizeof(StructLink));
+
 	while (fst->next != NULL)
-	{
 		fst = fst->next;
-	}
-	fst->next = temp; // fst становится следующим для
-	temp->field = number; // в буферный задаем поле
-	temp->next = NULL; // в буферный задаем следующий за ним элемент - p
-	temp->prev = fst; // в буферный задаем предыдущий ним элемент - fst
+
+	fst->next = temp;
+	temp->link = number;
+	temp->next = NULL;
+	temp->prev = fst;
 	return(temp);
 }
 
-struct StructLink* deletelemn(StructLink* fst) // удаляем min
+struct StructLink* DeleteMin(StructLink* fst)
 {
 	struct StructLink* p;
 	p = fst;
 	int a, b;
-	struct StructLink* temp; // буферный кусок
-	temp = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* temp;
+	temp = (struct StructLink*)malloc(sizeof(StructLink));
 	temp = fst;
-	a = temp->field;
+	a = temp->link;
 
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a > b)
 		{
 			temp = p;
@@ -82,40 +82,42 @@ struct StructLink* deletelemn(StructLink* fst) // удаляем min
 	p = fst;
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a == b)
 		{
 			temp = p;
 			p = p->next;
-			prevv = temp->prev; // узел, предшествующий lst
-			nextt = temp->next; // узел, следующий за lst
+			prevv = temp->prev;
+			nextt = temp->next;
 
 			if (prevv != NULL)
-				prevv->next = nextt; // переставляем указатель
+				prevv->next = nextt;
 			if (nextt != NULL)
-				nextt->prev = prevv; // переставляем указатель
+				nextt->prev = prevv;
 			if (temp == fst)
 				fst = fst->next;
-			free(temp); // освобождаем память удаляемого элемента
+			free(temp);
 		}
 		else
 			p = p->next;
 	}
+
 	return(fst);
 }
 
-struct StructLink* deletelemx(StructLink* fst) // удаляем min
+struct StructLink* DeleteMax(StructLink* fst)
 {
 	struct StructLink* p;
 	p = fst;
 	int a, b;
-	struct StructLink* temp; // буферный кусок
-	temp = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* temp;
+	temp = (struct StructLink*)malloc(sizeof(StructLink));
 	temp = fst;
-	a = temp->field;
+	a = temp->link;
+
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a < b)
 		{
 			temp = p;
@@ -128,105 +130,86 @@ struct StructLink* deletelemx(StructLink* fst) // удаляем min
 	p = fst;
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a == b)
 		{
 			temp = p;
 			p = p->next;
-			prevv = temp->prev; // узел, предшествующий lst
-			nextt = temp->next; // узел, следующий за lst
+			prevv = temp->prev;
+			nextt = temp->next;
+
 			if (temp == fst)
 				fst = fst->next;
 			if (prevv != NULL)
-				prevv->next = temp->next; // переставляем указатель
+				prevv->next = temp->next;
 			if (nextt != NULL)
-				nextt->prev = temp->prev; // переставляем указатель
-			free(temp); // освобождаем память удаляемого элемента
+				nextt->prev = temp->prev;
+
+			free(temp);
 		}
 		else
 			p = p->next;
 	}
+
 	return(fst);
 }
 
-struct StructLink* deletehead(StructLink* root) // удаляем корневой
+struct StructLink* deletehead(StructLink* root)
 {
 	struct StructLink* temp;
 	temp = root->next;
 	temp->prev = NULL;
-	free(root); // освобождение памяти текущего корня
-	return(temp); // новый корень списка
+	free(root);
+
+	return(temp);
 }
 
-void listprint(StructLink* lst) // печатаем нормально
-{
-	cout << endl;
-	struct StructLink* p;
-	p = lst;
-	do {
-		printf("%d ", p->field); // вывод значения элемента p
-		p = p->next; // переход к следующему узлу
-	} while (p != NULL); // условие окончания обхода
-	cout << endl << endl;
-}
-
-void listprintr(StructLink* lst) // печатаем задом наперед
-{
-	cout << endl;
-	struct StructLink* p;
-	p = lst;
-	while (p->next != NULL)
-		p = p->next; // переход к концу списка
-	do {
-		printf("%d ", p->field); // вывод значения элемента p
-		p = p->prev; // переход к предыдущему узлу
-	} while (p != NULL); // условие окончания обхода
-	cout << endl << endl;
-}
-
-struct StructLink* lswap(StructLink* fst) // взаимообмен файлов
+struct StructLink* SwapLink(StructLink* fst)
 {
 	struct StructLink* p;
 	p = fst;
 	int a, b;
-	struct StructLink* min; // буферный кусок
-	min = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* min;
+	min = (struct StructLink*)malloc(sizeof(StructLink));
 	min = fst;
-	a = min->field;
+	a = min->link;
+
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a > b)
 		{
 			min = p;
 			a = b;
 		}
 		p = p->next;
-	};
+	}
 
 	p = fst;
 
-	struct StructLink* max; // буферный кусок
-	max = (struct StructLink*)malloc(sizeof(StructLink)); // выделение памяти под буфер
+	struct StructLink* max;
+	max = (struct StructLink*)malloc(sizeof(StructLink));
 	max = fst;
-	a = max->field;
+	a = max->link;
+
 	while (p->next != NULL)
 	{
-		b = p->field;
+		b = p->link;
 		if (a < b)
 		{
 			max = p;
 			a = b;
 		}
 		p = p->next;
-	};
+	}
 
 	struct StructLink* prevn, * prevx, * nextn, * nextx;
-	prevn = min->prev; // узел предшествующий min
-	prevx = max->prev; // узел предшествующий max
-	nextn = min->next; // узел следующий за min
-	nextx = max->next; // узел следующий за max
-	if (max == nextn) // обмениваются соседние узлы
+	prevn = min->prev;
+	prevx = max->prev;
+	nextn = min->next;
+	nextx = max->next;
+
+	if (max == nextn)
 	{
 		max->next = min;
 		max->prev = prevn;
@@ -238,7 +221,7 @@ struct StructLink* lswap(StructLink* fst) // взаимообмен файлов
 			prevn->next = max;
 	}
 
-	else if (min == nextx) // обмениваются соседние узлы
+	else if (min == nextx)
 	{
 		min->next = max;
 		min->prev = prevx;
@@ -250,17 +233,17 @@ struct StructLink* lswap(StructLink* fst) // взаимообмен файлов
 			prevx->next = min;
 	}
 
-	else // обмениваются отстоящие узлы
+	else
 	{
-		if (min != fst) // указатель prev можно установить только для элемента,
-			prevn->next = max; // не являющегося корневым
+		if (min != fst)
+			prevn->next = max;
 		max->next = nextn;
 		if (max != fst)
 			prevx->next = min;
 		min->next = nextx;
 		max->prev = prevn;
-		if (nextx != NULL) // указатель next можно установить только для элемента,
-			nextx->prev = min; // не являющегося последним
+		if (nextx != NULL)
+			nextx->prev = min;
 		min->prev = prevx;
 		if (nextn != NULL)
 			nextn->prev = max;
@@ -275,87 +258,122 @@ struct StructLink* lswap(StructLink* fst) // взаимообмен файлов
 	return(fst);
 }
 
-int menue(int q)
+void PrintStruct(StructLink* lst)
 {
-	do
-	{
-		cout << "Выберите пунктик меню" << endl;
-		cout << "1. Добавить в начало;" << endl;
-		cout << "2. Добавить в конец;" << endl;
-		cout << "3. Удалить элемент с наименьшим значением;" << endl;
-		cout << "4. Удалить элемент с наибольшим значением;" << endl;
-		cout << "5. Поменять наибольший и наименьший элемент местами;" << endl;
-		cout << "6. Вывод списка по порядку;" << endl;
-		cout << "7. Вывод списка с конца;" << endl;
-		cout << "0. Выход." << endl;
-		cin >> q;
-	} while (q < 0 || q>7);
+	cout << endl;
+	struct StructLink* p;
+	p = lst;
 
-	return (q);
+	do {
+		printf("%d ", p->link);
+		p = p->next;
+	} while (p != NULL);
+
+	cout << endl << endl;
+}
+
+void PrintStruct_Reverse(StructLink* lst)
+{
+	cout << endl;
+	struct StructLink* p;
+	p = lst;
+
+	while (p->next != NULL)
+		p = p->next;
+
+	do {
+		printf("%d ", p->link);
+		p = p->prev;
+	} while (p != NULL);
+
+	cout << endl << endl;
+}
+
+int Menu(int task)
+{
+	do {
+		cout << "Выберите действие" << endl;
+		cout << "1. Добавление элемента в начало" << endl;
+		cout << "2. Добавление элемента в конец" << endl;
+		cout << "3. Удалить наименьший элемент" << endl;
+		cout << "4. Удалить наибольший элемент" << endl;
+		cout << "5. Поменять наибольший и наименьший элемент местами" << endl;
+		cout << "6. Вывод списка" << endl;
+		cout << "7. Вывод списка наоборот" << endl;
+		cout << "0. Выход" << endl;
+		cin >> task;
+	} while (task < 0 || task > 7);
+
+	return (task);
 }
 
 int main()
 {
 	setlocale(0, "");
+
 	cout << "Введите начальный элемент списка" << endl;
 	int a;
 	cin >> a;
-	struct StructLink* fst = inti(a);
-	int q = 9;
-	q = menue(q);
-	while (q >= 0 && q <= 7)
-	{
-		switch (q)
-		{
-		case 1:
-		{
-			fst = AddToStart(fst);
-			q = menue(q);
-			break;
-		}
-		case 2:
-		{
-			addingend(fst);
-			q = menue(q);
-			break;
-		}
-		case 3:
-		{
-			fst = deletelemn(fst);
-			q = menue(q);
-			break;
-		}
-		case 4:
-		{
-			fst = deletelemx(fst);
-			q = menue(q);
-			break;
-		}
-		case 5:
-		{
-			fst = lswap(fst);
-			q = menue(q);
-			break;
-		}
-		case 6:
-		{
-			listprint(fst);
-			q = menue(q);
-			break;
-		}
-		case 7:
-		{
-			listprintr(fst);
-			q = menue(q);
-			break;
-		}
-		case 0:
-		{
-			q = 10;
-			break;
-		}
 
-		default: menue(q);
+	struct StructLink* fst = StructInit(a);
+
+	int task = 9;
+	task = Menu(task);
+
+	while (task >= 0 && task <= 7)
+	{
+		switch (task)
+		{
+			case 1:
+			{
+				fst = AddToStart(fst);
+				task = Menu(task);
+				break;
+			}
+			case 2:
+			{
+				AddToEnd(fst);
+				task = Menu(task);
+				break;
+			}
+			case 3:
+			{
+				fst = DeleteMin(fst);
+				task = Menu(task);
+				break;
+			}
+			case 4:
+			{
+				fst = DeleteMax(fst);
+				task = Menu(task);
+				break;
+			}
+			case 5:
+			{
+				fst = SwapLink(fst);
+				task = Menu(task);
+				break;
+			}
+			case 6:
+			{
+				PrintStruct(fst);
+				task = Menu(task);
+				break;
+			}
+			case 7:
+			{
+				PrintStruct_Reverse(fst);
+				task = Menu(task);
+				break;
+			}
+			case 0:
+			{
+				task = 10;
+				break;
+			}
+
+			default: 
+				Menu(task);
 		}
 	}
 }
